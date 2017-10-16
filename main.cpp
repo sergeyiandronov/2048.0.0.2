f0f6d51754a6b5e8fcc0d964431ef6ae341b1639
sergeyiandronov <sergeyiandronov@gmail.com>
2017-10-16 19:19:52 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+int bmp[4][4];
+void initmas(int u[4]){
+    for(int i=0;i<4;i++){u[i]=0;}
+}
+void initmas(int u[4][4]){
+    for(int i=0;i<4;i++){for(int j=0;j<4;j++){u[i][j]=0;}}
+}
+
+bool comp(int a,int a1[4][4],int a2[4],char op){
+	bool res=true;
+	if(op=='x'){
+		for(int i=0;i<4;i++){if(a1[a][i]!=a2[i]){res=false;}}
+	}
+	if(op=='y'){
+		for(int i=0;i<4;i++){if(a1[i][a]!=a2[i]){res=false;}}
+	}
+	return res;
+}
+int ran2(int p){
+    int l=0;
+    int z=rand()%p;
+    if(z!=0){l=1;}
+    for(int i=0;i<z;i++){
+
+        l+=l;
+
+    }
+    return l;
+}
+
+
+void init(){
+    initmas(bmp);
+    srand(time(NULL));
+    bmp[1][0]=ran2(4);
+    bmp[1][1]=ran2(4);
+    bmp[1][2]=ran2(4);
+    bmp[1][3]=ran2(4);
+    bmp[2][3]=ran2(4);
+    bmp[0][3]=ran2(4);
+    bmp[2][2]=ran2(4);
+}
+bool  up(int uper[4][4]){
+    bool any=false;
+    
+
+    for(int j=0;j<4;j++){int y[4];initmas(y);int n=0;
+        for(int i=0;i<4;i++){if(uper[j][i]!=0){y[n]=uper[j][i];n++;}}
+          if(!comp(j,uper,y,'x')){any=true;}
+        for(int z=0;z<4;z++){if((y[z]==y[z+1])&&(y[z]!=0)){y[z]+=y[z+1];y[z+1]=0;any=true;}}
+
+        for(int w=0;w<3;w++){if(y[w]==0){y[w]=y[w+1];y[w+1]=0;}}
+
+        for(int i=0;i<4;i++){uper[j][i]=y[i];};
+    }
+
+   return any;
+}
+
+bool down(int uper[4][4]){
+     bool any=false;
+
+    for(int j=0;j<4;j++){int y[4];initmas(y);int n=3;
+        for(int i=3;i>-1;i--){if(uper[j][i]!=0){y[n]=uper[j][i];n--;}}
+      if(!comp(j,uper,y,'x')){any=true;}
+        for(int z=3;z>0;z--){if((y[z]==y[z-1])&&(y[z]!=0)){y[z]+=y[z-1];y[z-1]=0;any=true;}}
+
+        for(int w=3;w>0;w--){if(y[w]==0){y[w]=y[w-1];y[w-1]=0;}}
+
+        for(int i=0;i<4;i++){uper[j][i]=y[i];};
+    }
+
+  return any;
+}
+bool left(int uper[4][4]){
+     bool any=false;
+    for(int j=0;j<4;j++){int y[4];initmas(y);int n=0;
+        for(int i=0;i<4;i++){if(uper[i][j]!=0){y[n]=uper[i][j];n++;}}
+           if(!comp(j,uper,y,'y')){any=true;}
+        for(int z=0;z<4;z++){if((y[z]==y[z+1])&&(y[z]!=0)){y[z]+=y[z+1];y[z+1]=0;any=true;}}
+
+        for(int w=0;w<3;w++){if(y[w]==0){y[w]=y[w+1];y[w+1]=0;}}
+
+        for(int i=0;i<4;i++){uper[i][j]=y[i];};
+    }
+ return any;
+
+
+}
+bool right(int uper[4][4]){
+    bool any=false;
+    for(int j=0;j<4;j++){int y[4];initmas(y);int n=3;
+        for(int i=3;i>-1;i--){if(uper[i][j]!=0){y[n]=uper[i][j];n--;}}
+          if(!comp(j,uper,y,'y')){any=true;}
+        for(int z=3;z>0;z--){if((y[z]==y[z-1])&&(y[z]!=0)){y[z]+=y[z-1];y[z-1]=0;any=true;}}
+
+        for(int w=3;w>0;w--){if(y[w]==0){y[w]=y[w-1];y[w-1]=0;}}
+
+        for(int i=0;i<4;i++){uper[i][j]=y[i];};
+    }
+   return any;
+   
+}
+void bmpout(){
+    for(int j=0;j<4;j++){
+        for(int i=0;i<4;i++){
+            if(bmp[i][j]==0){cout<<"*"<<" ";continue;}cout<<bmp[i][j]<<" ";
+        } cout<<"\n";
+    }
+    cout<<"\n";
+}
+int main() {char com=' ';
+    init();
+    
+    while(com!='q'){
+
+        switch(com){
+            case 'j':
+              cout << down(bmp)<<"\n";
+                break;
+            case 'k':
+                cout<<up(bmp)<<"\n";
+                break;
+            case 'h':
+                cout<<left(bmp)<<"\n";
+                break;
+            case 'l':
+                cout<<right(bmp)<<"\n";
+                break;
+
+        }
+        bmpout();
+        cin>>com;
+    }
+    return 0;
+}
